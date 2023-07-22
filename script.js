a1118d2bf0d147f1d7055b5fcf97873a57b16e19
viet_v <vohoangviet459@gmail.com>
2023-07-22 22:40:33 +0800

    Understand js class (constructor and method), Add comment to explain, Adjust canvas size

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const particlesArray = [];
 
 console.log(ctx);
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight / 2;
 
 window.addEventListener("resize", function() {
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.height = window.innerHeight / 2;
     
 })
 
@@ -17,41 +17,23 @@ const mouse = {
     y: undefined
 }
 
-// function drawCircle() {
-//     ctx.fillStyle = "white";
-//     ctx.strokeStyle = "orange";
-//     ctx.lineWidth = 5;
-//     ctx.beginPath();
-//     ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-//     ctx.fill();
-//     ctx.stroke();
-// }
-// drawCircle();
-
 
 canvas.addEventListener("click", function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
-    // for (let i = 0; i < 10; i++) {
-    //     particlesArray.push(new Particle());   
-    // }
-    // drawCircle();
+
 })
 
 canvas.addEventListener("mousemove", function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
-    // for (let i = 0; i < 1; i++) {
-    //     particlesArray.push(new Particle());   
-    // }
-    // drawCircle();
+
 })
 
 
 class Particle {
     constructor() {
-        // this.x = mouse.x;
-        // this.y = mouse.y;
+        // Random Position, Size and Speed of Each Particle
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
         this.size = Math.random() * 5 + 1;
@@ -59,15 +41,13 @@ class Particle {
         this.speedY = Math.random() * 3 - 1.5;
     }
     update() {
+        // Make The Particle Move
         this.x += this.speedX / 3;
         this.y += this.speedY / 3;
-        // if ((this.x == canvas.width)) {
-        //     // this.x = this.x * -1;
-        //     console.log(this.x)
-        // }
-        // if (this.size > 0.2) this.size -= 0.1; 
+
     }
     draw() {
+        // How The Particle Looks
         ctx.fillStyle = "orange";
         ctx.strokeStyle = "orange";
         ctx.lineWidth = 1/6;
@@ -78,6 +58,7 @@ class Particle {
     }
 }
 
+// Initiate a Number of Particles (constructor is called)
 function init() {
     for (let i = 0; i < 150; i++) {
         particlesArray.push(new Particle());
@@ -85,11 +66,16 @@ function init() {
 }
 init();
 
-
+// Call update method to make particle move, draw method to draw each particle
+// And use Pythagorean Theorem to create connecting lines
 function handleParticles() {
     for (let i = 0; i < particlesArray.length; i++) {
+
+        // Make Particles Move
         particlesArray[i].update();
+        // Draw Each Particle
         particlesArray[i].draw();
+
         for (let j = i; j < particlesArray.length; j++) {
             // Connecting The Dots
             const dx = particlesArray[i].x - particlesArray[j].x;
@@ -125,17 +111,13 @@ function handleParticles() {
         if ((particlesArray[i].y >= canvas.height) || (particlesArray[i].y <= 0)) {
             particlesArray[i].speedY *= -1;
         }
-        // if (particlesArray[i].size < 0.3) {
-        //     particlesArray.splice(i, 1);
-        //     i--;
-        // }
+
     }
 }
 
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // drawCircle();
     handleParticles();
     requestAnimationFrame(animate);
 }
